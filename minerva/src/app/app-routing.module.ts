410c4624326d3456282a69f068c9bcf9ba45ae3b
RicardoBorgesPack <ricardovborges2000@gmail.com>
2020-09-14 19:23:32 -0300

    Create "feed"

--- minerva/src/app/app-routing.module.ts
+++ minerva/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './Pages/home/home.component';
 import { TeatcherComponent } from './Pages/teatcher/teatcher.component';
 import { ClassesComponent } from './Pages/classes/classes.component';
+import { CardComponent } from './UI/card/card.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'teatch', component: TeatcherComponent },
-  { path: 'study', component:  ClassesComponent}
+  { path: 'study', component:  ClassesComponent},
+  { path: 'feed', component:  CardComponent}
 ];
 
 @NgModule({
