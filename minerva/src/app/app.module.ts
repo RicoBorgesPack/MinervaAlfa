69a3ae808c75e2fe9e6e771bf625f5ba8ebaa8de
RicardoBorgesPack <ricardovborges2000@gmail.com>
2020-09-14 19:21:11 -0300

    Add card + carrousel font

--- minerva/src/app/app.module.ts
+++ minerva/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule, MatCard} from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +12,7 @@ import { HomeComponent } from './Pages/home/home.component';
 import { TeatcherComponent } from './Pages/teatcher/teatcher.component';
 import { CarrouselComponent } from './UI/carrousel/carrousel.component';
 import { ClassesComponent } from './Pages/classes/classes.component';
+import { CardComponent } from './UI/card/card.component';
 
 @NgModule({
   declarations: [
@@ -19,13 +21,15 @@ import { ClassesComponent } from './Pages/classes/classes.component';
     HomeComponent,
     TeatcherComponent,
     CarrouselComponent,
-    ClassesComponent
+    ClassesComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatIconModule
+    MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
