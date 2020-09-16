373f8a2a33b6b2ad891edfcc4f0a72818287926f
RicardoBorgesPack <ricardovborges2000@gmail.com>
2020-09-16 12:21:37 -0300

    First Firebase

--- minerva/src/environments/environment.prod.ts
+++ minerva/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAgBOwz6gmZGvQCvnkUzRWUPx48mnJD6gs",
+    authDomain: "minerva-spark.firebaseapp.com",
+    databaseURL: "https://minerva-spark.firebaseio.com",
+    projectId: "minerva-spark",
+    storageBucket: "minerva-spark.appspot.com",
+    messagingSenderId: "1041446456421",
+    appId: "1:1041446456421:web:37910883ddc3457183ca8c",
+    measurementId: "G-5ZBWY7008E"
+  }
 };
