c4ddbdc1256cf94316ec99212ed4213abeeb8238
RicardoBorgesPack <ricardovborges2000@gmail.com>
2020-10-16 11:22:17 -0300

    Conexão banco de json

--- minerva/src/app/Pages/classes/classes.component.ts
+++ minerva/src/app/Pages/classes/classes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-classes',
@@ -6,11 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./classes.component.css']
 })
 export class ClassesComponent implements OnInit {
+  teacher$ = this.http.get('http://localhost:3000/Teacher');
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     
-  }
+  }/*teams$ = this.http.get('http://localhost:3000/Teacher');
+
+  constructor(private http: HttpClient) {}*/
 
 }
