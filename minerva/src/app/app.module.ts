8eb2c58c01f86af270282d207b87ff4f816071f6
RicardoBorgesPack <ricardovborges2000@gmail.com>
2020-10-22 15:30:04 -0300

    Pasta Shared

--- minerva/src/app/app.module.ts
+++ minerva/src/app/app.module.ts
@@ -16,7 +16,7 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './UI/header/header.component';
+import { HeaderComponent } from './shared/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './Pages/home/home.component';
 import { TeacherFormComponent } from './Pages/teatcher/teatcher.component';
