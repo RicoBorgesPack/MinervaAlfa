61c0d883c5730fd9eb60c10990e621fdaad47b7c
RicardoBorgesPack <ricardovborges2000@gmail.com>
2020-11-03 15:18:45 -0300

    Insert 3d scene

--- minerva/src/app/Pages/classes/classes.component.ts
+++ minerva/src/app/Pages/classes/classes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ConfigService} from "src/app/Services/config.service";
+import { Teacher } from 'src/app/Model/teacher';
 
 @Component({
   selector: 'app-classes',
@@ -8,13 +10,21 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ClassesComponent implements OnInit {
   teacher$ = this.http.get('http://localhost:3000/Teacher');
+  teacher = new Teacher();
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     
-  }/*teams$ = this.http.get('http://localhost:3000/Teacher');
-
-  constructor(private http: HttpClient) {}*/
-
+  }
+  getLinkWhastapp(number, message) {
+    var url = 'https://api.whatsapp.com/send?phone=' 
+       + number 
+       + '&text=' 
+       + encodeURIComponent(message)
+  
+    return url}
+    onsubmit(){
+      const contact = (document.getElementById(this.teacher)as any).value;
+    }
 }
